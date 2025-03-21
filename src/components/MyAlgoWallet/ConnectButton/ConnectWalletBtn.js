49a8ef4deddbc7338533a00fb94e959aec45a9bb
nabeelhassankhan19 <nabeelkhan@decryptedlabs.io>
2025-03-21 14:09:15 +0500

    Update Package.json Ahmed

--- src/components/MyAlgoWallet/ConnectButton/ConnectWalletBtn.js
+++ src/components/MyAlgoWallet/ConnectButton/ConnectWalletBtn.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import { loadStdlib } from '@reach-sh/stdlib'
-import MyAlgoConnect from '@reach-sh/stdlib/ALGO_MyAlgoConnect';
+import React from 'react';
+import { loadStdlib } from '@reach-sh/stdlib';
 import ConnectButton from '.';
 import { BodyText } from '../MyAlgoWallet.styles';
-const reach = loadStdlib("ALGO")
-reach.setWalletFallback(reach.walletFallback({
-    providerEnv: 'TestNet', MyAlgoConnect
-}));
 
+const reach = loadStdlib('ALGO');
+
+// Correct way to get MyAlgoConnect
+const MyAlgoConnect = reach.walletFallback({
+    providerEnv: 'TestNet',
+    MyAlgoConnect: reach.ALGO_MyAlgoConnect
+});
+
+reach.setWalletFallback(MyAlgoConnect);
 
 const ConnectWalletButton = ({ connectWallet, accountAddress, accountBal }) => {
-
     return (
         <div>
             <ConnectButton connectWallet={connectWallet} />
             <BodyText>Account Address: {accountAddress} </BodyText>
             <BodyText>Account Balance: {accountBal}</BodyText>
         </div>
-    )
-}
+    );
+};
 
-export default ConnectWalletButton
+export default ConnectWalletButton;
